abc16ed6dfcf84cbca13ba2a4b3319217e0da020
GUSTAVO ADOLFO MENA <gustavo.mena@mercadolibre.com>
2019-11-05 20:05:41 -0300

    Change DB connection

--- timer-ml/clases/Conexion.php
+++ timer-ml/clases/Conexion.php
@@ -50,11 +50,11 @@ class Conexion
 	static function conectarK()
 		{
 			$opciones  = array(\PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8");
-			$link = new \PDO(
-				"mysql:host=localhost;dbname=mconnect_basegeneral",
-				"mconnect_cliente", //usuario
-				"4pwUzorS)xl#", //pass
-				$opciones);
+			$link = new PDO(
+				"mysql:host=localhost;dbname=u430849600_mconnecting",
+				"u430849600_mcon", //usuario
+				"Password0", //pass
+				$opciones);	
 
 			$link->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_WARNING);
 			return $link;
